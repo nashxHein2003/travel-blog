378647785c2e84f6bc3a19bedc39032d39929db7
Thurain <thuruhe557@gmail.com>
2023-12-15 14:50:37 +0630

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default function About () {
   return (
     <>
-      <section class="section_about">
-        <div class="about">
-          <div class="about--text">
+      <section className="section_about">
+        <div className="about">
+          <div className="about--text">
             <h1>Find Your Pleasure <br /> by <span>exploring</span> the world</h1>
             <p>
               Exploring the world through travel is an exhilarating journey of discovery. It's about embracing diverse cultures, savoring unique flavors, and creating lasting memories. Each adventure unfolds a new chapter, turning the ordinary into extraordinary experiences that linger in the heart forever.</p>
 
-              <div class="about--text_line"></div>
+              <div className="about--text_line"></div>
           </div>
-          <div class="about--img">
+          <div className="about--img">
             
             <img src="./../image/about/about--img1.jpg" class="about--img1" />
             <img src="./../image/about/about--img2.jpg" alt="" class="about--img2" />
